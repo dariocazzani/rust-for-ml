9f2099ec01fd5eb8002b2d21c528ae4d3dbb59be
dariocazzani <dariocazzani@gmail.com>
2023-04-09 14:32:46 -0400

    Run reverse in one line

--- src/chapter02/exercise4.rs
+++ src/chapter02/exercise4.rs
@@ -2,16 +2,16 @@ use std::io;
 use std::io::Write;
 use std::io::stdout;
 
-fn reverse_string(input_string: & String) -> String {
-    let mut new_string:String = String::new();
-    new_string.extend(input_string.chars().rev());
-    return new_string;
+fn reverse_string(input_string: &String) -> String {
+    input_string.chars().rev().collect::<String>()
 } 
 
 pub fn run() {
     print!("Write a message to reverse: ");
     stdout().flush().expect("Could not flush output");
+    
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).expect("Could not read line");
-    println!("The reversed string is: {}", reverse_string(&buffer));
+    
+    println!("The reversed string is: {}", reverse_string(&mut buffer));
 }
